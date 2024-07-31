71a5a4b46797da389867842445bf7e9f11f44eb3
Gavin <gurven-mckruger@hotmail.co.uk>
2024-07-31 15:16:29 +0100

    Update style for button, update meta info

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0311493ede1fda6d81a0');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ebc37dd2b2deac3c2dd0');
--- build/render.php
+++ build/render.php
@@ -8,22 +8,22 @@
 	<form action="/contact-form-submission/" method="post">
 		<div class="form-row">
 			<label for="name">Full Name:</label>
-			<input type="text" id="name" name="name" required>
+			<input class="form-input" type="text" id="name" name="name" required>
 		</div>
 		<div class="form-row">
 			<label for="email">Email Address:</label>
-			<input type="email" id="email" name="email" required>
+			<input class="form-input" type="email" id="email" name="email" required>
 		</div>
 		<div class="form-row">
 			<label for="phone">Phone Number:</label>
-			<input type="phone" id="phone" name="phone" required>
+			<input class="form-input" type="phone" id="phone" name="phone" required>
 		</div>
 		<div class="form-row">
 			<label for="message">Message:</label>
-			<textarea id="message" name="message" required></textarea>
+			<textarea class="form-input" id="message" name="message" required></textarea>
 		</div>
 		<div class="form-row">
-			<input class="btn-submit" type="submit" id="form-submit" name="form-submit" value="Submit">
+			<input class="wp-block-button__link wp-element-button" type="submit" id="form-submit" name="form-submit" value="Submit">
 		</div>
 	</form>
 </div>
